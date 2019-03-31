145c47b03df49bdb1b545aa1aee8b69603e07ef2
Natasha Chernis <natashachernis@gmail.com>
2019-03-31 04:22:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,12 +2,7 @@ require 'pry'
 
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
-#  new_arr = []
-#  alphabet.each_with_index {|letter, index|
-#    new_arr << arr.map {|word| word = index.to_s + word if word[0] == letter}
-#  }
-#  binding.pry
-binding.pry
+
 new_arr = []
 arr.each {|phrase|
   alphabet.each_with_index {|letter, index|
