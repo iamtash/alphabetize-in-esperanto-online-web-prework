fc3fa402b1493106c091e0e51de7a338ce7aea36
Natasha Chernis <natashachernis@gmail.com>
2019-03-31 04:52:51 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,16 +1,11 @@
 require 'pry'
 
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+end
 
-  new_arr = []
-  arr.each {|phrase|
-    alphabet.each_with_index {|letter, index|
-      new_arr << [index, phrase] if phrase[0] == letter
-    }
-  }
-  #binding.pry
 
-  new_arr.sort_by {|phrase| phrase[0]}.map {|num_phrase| num_phrase[1]}
+
 
 end
