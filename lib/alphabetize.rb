3402be8248ee224e11a93cde4e1d423f66f4ad90
Natasha Chernis <natashachernis@gmail.com>
2019-03-31 04:02:56 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,5 +3,7 @@ require 'pry'
 def alphabetize(arr)
   binding.pry
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
-
+  alphabet.each_with_index {|letter, index|
+    arr.split(" ").map {|word| word = index.to_s + word if word[0] == letter}
+  }
 end
